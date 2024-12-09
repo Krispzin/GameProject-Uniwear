816bf9101fcac27dc6f9cfd1dc9c4ba47fdadf59
ikiew8520 <ikiew_555@hotmail.com>
2024-12-09 13:40:16 +0700

    Add MovingBar

---
+++ Game1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using ThanaNita.MonoGameTnt;
+
+namespace Project2
+{
+    public class Game1 : Game2D
+    {
+        public Game1()
+            : base(virtualScreenSize: new Vector2(640, 480),
+                  preferredWindowSize: new Vector2(640, 480))
+        {
+            BackgroundColor = Color.White;
+            IsFixedTimeStep = false;
+        }
+
+        protected override void LoadContent()
+        {
+            All.Add(new MovingBar(All, new Vector2(30, 420)));
+        }
+    }
+}
---
+++ MovingBar.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanaNita.MonoGameTnt;
+
+namespace Project2
+{
+    public class MovingBar : RectangleActor
+    {
+        public MovingBar(Actor all, Vector2 ScreenSize)
+            : base(Color.Black, new Vector2(8, 80))
+        {
+            Origin = RawSize / 2;
+            Position = ScreenSize;
+
+            AddAction(new Mover(this, new Vector2(300, 0)));
+
+            var collisionObj = CollisionObj.CreateWithRect(this, 1);
+            collisionObj.Position = OnCollide;
+            Add(collisionObj);
+            collisionObj.DebugDraw = true;
+        }
+
+        public void OnCollide(CollisionObj objB, CollisionObj data)
+        {
+            var hitBar = objB.Actor as hitBar;
+            hitBar?.Detach();
+        }
+    }
+}
