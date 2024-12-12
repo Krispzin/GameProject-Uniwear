aeb91b9a8eb512a59717fb7f7de3232af5b4010e
IPsBs <atikanps@gmail.com>
2024-12-12 15:58:39 +0700

    first commit

---
+++ Project2/Game1.cs
@@ -0,0 +1,47 @@
+﻿using Game12;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using ThanaNita.MonoGameTnt;
+
+namespace Project2
+{
+    public class Game1 : Game2D
+    {
+        Actor MenuScreen;
+        Actor inputN;
+            public Game1()
+                : base(virtualScreenSize: new Vector2(640, 480),
+                       preferredWindowSize: new Vector2(640, 480))
+        {
+            BackgroundColor = Color.DarkGray;
+            IsFixedTimeStep = false;
+        }
+        protected override void LoadContent()
+        {
+            MenuScreen = new MenuScreen(ScreenSize, ExitNotifier);
+            All.Add(MenuScreen);
+        }
+
+        private void ExitNotifier(Actor actor, int code)
+        {
+            if (actor == null)
+                return;
+
+            if (actor == MenuScreen)
+            {
+                MenuScreen.Detach();
+                MenuScreen = null;
+                inputN = new inputN(ExitNotifier);
+                All.Add(inputN);
+            }
+            else if (actor == inputN)
+            {
+                inputN.Detach();
+                inputN = null;
+                inputN = new MenuScreen(ScreenSize, ExitNotifier);
+                All.Add(MenuScreen);
+            }
+
+        }
+    }
+}
---
+++ Project2/MenuScreen.cs
@@ -0,0 +1,53 @@
+﻿
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using ThanaNita.MonoGameTnt;
+
+namespace Game12
+{
+  
+
+        public class MenuScreen : Actor
+        {
+            ExitNotifier exitNotifier;
+        public MenuScreen(Vector2 screenSize, ExitNotifier exitNotifier)
+        {
+            this.exitNotifier = exitNotifier;
+
+            var button1 = new Button("Poppins-Medium.ttf", 50, Color.Black, "Click Me", new Vector2(300, 100));
+
+            button1.Position = new Vector2(screenSize.X / 2 - button1.RawSize.X / 2, screenSize.Y / 2 - button1.RawSize.Y / 2);
+
+            Add(button1);
+            /*
+
+                var button2 = new Button("Poppins-Medium.ttf", 50, Color.Black, "kuy", new Vector2(300, 100));
+
+                button2.Position = new Vector2(screenSize.X / 2 - button2.RawSize.X / 2, screenSize.Y / 2 - button2.RawSize.Y / 2);
+
+                Add(button2);
+            }
+            */
+        }
+            private Actor selection = null;
+            private Vector2 click;
+            public override void Act(float deltaTime)
+            {
+                base.Act(deltaTime);
+
+                var mouseInfo = GlobalMouseInfo.Value;
+                var keyInfo = GlobalKeyboardInfo.Value;
+
+                if (mouseInfo.IsLeftButtonPressed())
+                    AddAction(new SequenceAction(
+                                    Actions.FadeOut(0.2f, this),
+                                    new RunAction(() => exitNotifier(this, 0))
+                        ));
+
+            }
+        }
+    }
+
+        
+        
+    
