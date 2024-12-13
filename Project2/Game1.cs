4cd56228a6768a0800e6534e8f9284529702aea7
IPsBs <atikanps@gmail.com>
2024-12-13 16:09:04 +0700

    Add song

--- Project2/Game1.cs
+++ Project2/Game1.cs
@@ -1,12 +1,15 @@
-﻿using Game12;
+﻿using System;
+using Game12;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using ThanaNita.MonoGameTnt;
 
 namespace Project2
 {
     public class Game1 : Game2D
     {
+        Song song;
         Actor credit;
         Actor menuScreen;
         Actor inputScreen;
@@ -22,9 +25,19 @@ namespace Project2
             menuScreen = new MenuScreen(ScreenSize, ExitNotifier);
             All.Add(menuScreen);
 
-
+            song = Song.FromUri(name: "Song01",
+                new Uri("Pikuniku Dancing - Extended Version [TubeRipper.cc].ogg", UriKind.Relative));
+            MediaPlayer.Play(song);
+            //  Uncomment the following line will also loop the song
+            MediaPlayer.IsRepeating = true;
+            MediaPlayer.MediaStateChanged += MediaPlayer_MediaStateChanged;
         }
-
+        void MediaPlayer_MediaStateChanged(object sender, System.EventArgs e)
+        {
+            // 0.0f is silent, 1.0f is full volume
+            MediaPlayer.Volume -= 0.0f;
+            MediaPlayer.Play(song);
+        }
         private void ExitNotifier(Actor actor, int code)
         {
             if (actor == null)
