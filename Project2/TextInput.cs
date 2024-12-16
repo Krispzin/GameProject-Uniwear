d762ab5639cf3caa3afd30257c216b6c9c736700
ryuch <ryuch@RYUCHAYANN>
2024-12-16 20:45:18 +0700

    wwww

---
+++ Project2/TextInput.cs
@@ -0,0 +1,40 @@
+﻿using Game12;
+using Microsoft.Xna.Framework;
+using System.Runtime.InteropServices;
+using ThanaNita.MonoGameTnt;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project2
+{
+    public class TextInput : Actor 
+    {
+        Text text;
+        ExitNotifier exitNotifier;
+        public TextInput(Vector2 ScreenSize, ExitNotifier exitNotifier)
+        {
+            
+
+
+
+        }
+
+        public override void Act(float deltaTime)
+        {
+            base.Act(deltaTime);
+            var keyInfo = GlobalKeyboardInfo.Value;
+            if (keyInfo.IsKeyPressed(Keys.Enter))
+            {
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
